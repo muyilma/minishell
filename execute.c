f2c48422fba7003cba0e00fb7a86d7f7424d6749
Ömer Faruk Göregen <omgorege@k1m37s03.42istanbul.com.tr>
2025-05-01 17:16:43 +0300

    built_in dosyası aktif

--- execute.c
+++ execute.c
@@ -8,30 +8,6 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-
-void	ft_echo(char **args)
-{
-	int i;
-	int newline;
-
-	newline = 1;
-	i = 0;
-	if (args[0] && ft_strncmp(args[0], "-n", 3) == 0)
-	{
-		newline = 0;
-		i = 1;
-	}
-	while (args[i])
-	{
-		printf("%s", args[i]);
-		if (args[i + 1])
-			printf(" ");
-		i++;
-	}
-	if (newline)
-		printf("\n");
-}
-
 void	ft_free(char **str)
 {
 	int i;
