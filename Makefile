66962d6dd09fcff556f25ca9fc2561282e8c260b
Omgorege <omgorege@example.com>
2025-05-15 12:40:04 +0300

    deneme pushu

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 NAME=minishell
 
 
-SRCS= minishell.c parser.c dollar_parse.c arg_parse.c arg_parse2.c execute.c built_in.c file_redirect.c env.c export.c cd.c ft_free.c echo_and_unset.c exit.c
+SRCS= minishell.c parser.c dollar_parse.c arg_parse.c arg_parse2.c execute.c built_in.c file_redirect.c env.c export.c cd.c ft_free.c echo_and_unset.c exit.c env_and_pwd.c
 
 CFLAGS=-g
 LIBFT=libft/libft.a
---
+++ env_and_pwd.c
@@ -0,0 +1,40 @@
+#include "libft/libft.h"
+#include "minishell.h"
+#include <fcntl.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void	ft_pwd(void)
+{
+	char	cwd[1024];
+
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+		printf("%s\n", cwd);
+	else
+		ft_print_error(NULL,"pwd not found", NULL, 1);
+}
+
+void	ft_env(char **env, char **args, t_input *pro)
+{
+	int i;
+
+	i = 0;
+	if (args && args[0])
+	{
+		if (ft_strchr(args[0], '=') == NULL)
+		{
+			perror("env:");
+			exit(0);
+		}
+		ft_export(&args[0], pro, 1);
+	}
+	while (env[i])
+	{
+		if (ft_strchr(env[i], '='))
+			printf("%s\n", env[i]);
+		i++;
+	}
+}
